130772ba8965b615b991cd31f73f046260f8cc25
Henrique Althman <henriquealthman23@gmail.com>
2025-05-13 08:49:47 -0300

    Update Exercício3.cpp

--- Exercício3.cpp
+++ Exercício3.cpp
@@ -6,8 +6,10 @@ int main(){
     scanf("%f", &not1);
     printf("Digite a segunda nota: ");
     scanf("%f", &not2);
+    //aqui eu somei as notas e fiz as medias//
     som = (not1 + not2);
     med = som / 2;
+    // aqui eu fiz um se não se, para verificar se ele foi aprovado//
     if (med >= 7)
     {
         printf("Você foi aprovado: %2.f", med);
