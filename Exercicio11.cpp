c653933015b69a8400404734df5161ea6a20e003
Henrique Althman <henriquealthman23@gmail.com>
2025-05-13 13:44:46 -0300

    Update Exercicio11.cpp

--- Exercicio11.cpp
+++ Exercicio11.cpp
@@ -1,17 +1,1 @@
-#include <stdio.h>
 
-int main() {
-    int numeros[10];
-    int soma = 0;
-
-    for (int i = 0; i < 10; i++) {
-        printf("Digite o %dº número: ", i + 1);
-        scanf("%d", &numeros[i]);
-        soma += numeros[i];
-    }
-
-    int media = soma / 10;
-    printf("A média é: %d\n", media);
-
-    return 0;
-}
