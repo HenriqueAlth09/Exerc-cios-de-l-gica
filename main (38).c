ab0939b22e672eb10625c801bcc761c9191b1519
Henrique Althman <henriquealthman23@gmail.com>
2025-05-13 11:13:00 -0300

    Add files via upload

---
+++ main (38).c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main(){
+    int opcao;
+    int num1, num2;
+     do{
+         printf("Menu:\n");
+         printf("1-Somar\n");
+         printf("2-Subtrair\n");
+         printf("3-Sair\n");
+         scanf("%d", &opcao);
+         
+         switch (opcao){
+             case 1:
+             printf("Digite o primeiro numero: ");
+             scanf("%d", &num1);
+             printf("Digite o segundo numero: ");
+             scanf("%d", &num2);
+             printf("O resultado é: %d\n", num1 + num2);
+             break;
+             
+             case 2:
+             printf("Digite o primeiro numero: ");
+             scanf("%d", &num1);
+             printf("Digite o segundo numero: ");
+             scanf("%d", &num2);
+             printf("O resultado é: %d\n", num1 - num2);
+             break;
+             
+             case 3:
+             printf("Saindo...");
+             break;
+             
+             default:
+             printf("Opção invalida!\n");
+         }
+    } while (opcao != 3);
+
+    return 0;
+}
+      
