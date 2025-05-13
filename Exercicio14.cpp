3fd552a5126e684eed5d7ed12e4f85909355786f
Henrique Althman <henriquealthman23@gmail.com>
2025-05-13 13:45:44 -0300

    Update Exercicio14.cpp

--- Exercicio14.cpp
+++ Exercicio14.cpp
@@ -1,19 +1,1 @@
-#include <stdio.h>
 
-int main() {
-    int vetor[5];
-
-    // Ler os números
-    for (int i = 0; i < 5; i++) {
-        printf("Digite o número: ");
-        scanf("%d", &vetor);
-    }
-
-    // Imprimir o vetor invertido
-    printf("Vetor invertido: ");
-    for (int i = 4; i >= 0; i--) {
-        printf("%d ", vetor);
-    }
-
-    return 0;
-}
